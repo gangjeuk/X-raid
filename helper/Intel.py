da5d2cdf2d8b5b8b92627d3827d6e8a9e12c9d91
gangjeuk <gangjeuk@gmail.com>
2024-06-04 02:07:40 +0900

    Update Intel.py
    Updated intel reconstruction helper.
    
    Added handling code for unexceptional cases.

--- helper/Intel.py
+++ helper/Intel.py
@@ -3,9 +3,8 @@ import os
 
 SECTOR_SIZE = 0x200
 
-def check_magic(data) -> bool:
-    magic = b'EFI PART'
-    # check magic values: EFI and BR
+def check_magic(data, magic) -> bool:
+    # check magic values
     if data[:len(magic)] == magic:
         return True
     elif data[-2:] == b'\x55\xAA':
@@ -18,37 +17,50 @@ def disk_order_helper(file_names):
     
     for file_name in file_names:
         with open(file_name, 'rb') as f:
-            for i in range(0x100):
-                if check_magic(f.read(SECTOR_SIZE)):
-                    print("Found Partition Record. First disk: {}, Offset: {}".format(file_name, hex(i * SECTOR_SIZE)))
+            for i in range(0x10000):
+                read_data = f.read(SECTOR_SIZE)
+                # check EFI
+                if check_magic(read_data, b'EFI PART'):
+                    print("Found GPT Partition Signature. First disk: {}, Offset: {}".format(file_name, hex(i * SECTOR_SIZE)))
                     dev_map[file_name]["order"] = 1
+                # check NTFS
+                elif check_magic(read_data, b'\xEB\x52\x90NTFS'):
+                    print("Found NTFS Signature. First disk: {}, Offset: {}".format(file_name, hex(i * SECTOR_SIZE)))
+                    dev_map[file_name]["order"] = 1              
     return dev_map
 
 def raid_level_helper(file_names, dev_map):
     print("Checking RAID level...")
     # We don't need think about RAID1 
+    # If all disk have order == -1, something went wrong in previous process
+    if len(list(filter(lambda x: dev_map[x]["order"] == -1, dev_map))) == len(dev_map):
+        print("Something went wrong. RAID level has not been set in previous step")
+        exit(1)
     # Only one disk should have EFI or BR. If not, it must be RAID1
+    print(dev_map)
     if len(list(filter(lambda x: dev_map[x]["order"] == 1, dev_map))) != 1:
         if len(file_names) == 2:
             print("RAID level must be 1. Check image files")
             exit(1)
-        elif len(list(filter(lambda x: dev_map[x]["order"] == 1, dev_map))) == 2 and len(file_names) == 4:
-            print("Guessed RAID level: RAID10")
-            return
         else:
             print("Something went wrong. Check image files")
             print("Manual Reconstruction might be needed")
             exit(1)
+    elif len(list(filter(lambda x: dev_map[x]["order"] == 1, dev_map))) == 2 and len(file_names) == 4:
+        print("Guessed RAID level: RAID10")
+        return
+
     
     # check RAID5
-    data = b'\x00' * SECTOR_SIZE
-    for file_name in file_names:
-        with open(file_name, 'rb') as f:
-            f.seek(0x200, os.SEEK_SET)
-            data = bytes(i ^ j for i, j in zip(data, f.read(SECTOR_SIZE)))
-    if data == b'\x00' * SECTOR_SIZE:
-        print("Gussed RAID level: RAID5")
-        return
+    if len(dev_map) >= 3:
+        data = b'\x00' * SECTOR_SIZE
+        for file_name in file_names:
+            with open(file_name, 'rb') as f:
+                f.seek(0x200, os.SEEK_SET)
+                data = bytes(i ^ j for i, j in zip(data, f.read(SECTOR_SIZE)))
+        if data == b'\x00' * SECTOR_SIZE:
+            print("Gussed RAID level: RAID5")
+            return
     
     print("Gussed RAID level: RAID0")
     return
@@ -64,7 +76,7 @@ def start_offset_helper(file_name):
             data = f.read(SECTOR_SIZE)
             if len(data) != SECTOR_SIZE:
                 break
-            if check_magic(data):
+            if check_magic(data, b'EFI PART'):
                 # fit in stripe unit size
                 start_offset_candi.add(((i * SECTOR_SIZE) // 0x1000) * 0x1000)
             i += 1
